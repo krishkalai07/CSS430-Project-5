38a6f56ddfb7fe2013256d187d926833b82c6ab4
Donghee Lee <heedong0612@gmail.com>
2020-03-12 23:44:58 -0700

    fixed minor indentation

--- ThreadOS/Inode.java
+++ ThreadOS/Inode.java
@@ -1,27 +1,24 @@
-import java.sql.Driver;
-
 public class Inode {
-    private final static int iNodeSize = 32;       // fix to 32 bytes
-    private final static int directSize = 11;      // # direct pointers
- 
-    public int length;                             // file size in bytes
-    public short count;                            // # file-table entries pointing to this
-    public short flag;                             // 0 = unused, 1 = used, ...
-    public short direct[] = new short[directSize]; // direct pointers
-    public short indirect;                         // a indirect pointer
- 
-    Inode() {                                     // a default constructor
-       length = 0;
-       count = 0;
-       flag = 1;
-       for ( int i = 0; i < directSize; i++ ) {
-           direct[i] = -1;
-       }
-       indirect = -1;
-    }
- 
-    Inode(short iNumber) {                       // retrieving inode from disk
+   private final static int iNodeSize = 32;       // fix to 32 bytes
+   private final static int directSize = 11;      // # direct pointers
 
+   public int length;                             // file size in bytes
+   public short count;                            // # file-table entries pointing to this
+   public short flag;                             // 0 = unused, 1 = used, ...
+   public short direct[] = new short[directSize]; // direct pointers
+   public short indirect;                         // a indirect pointer
+ 
+   Inode() {                                     // a default constructor
+      length = 0;
+      count = 0;
+      flag = 1;
+      for ( int i = 0; i < directSize; i++ ) {
+         direct[i] = -1;
+      }
+      indirect = -1;
+   }
+ 
+   Inode(short iNumber) {                       // retrieving inode from disk
       int blockNumber = 1 + iNumber / 16;
       int iNodeIndex = iNumber % 16;
 
@@ -44,12 +41,10 @@ public class Inode {
       for (int i = 0; i < directSize; i++, offset += 2) {
          direct[i] = SysLib.bytes2short(barr, offset);
       }
-
       indirect = SysLib.bytes2short(barr, offset);
-
     }
  
-    int toDisk(short iNumber) {                  // save to disk as the i-th inode
+   int toDisk(short iNumber) {                  // save to disk as the i-th inode
        // design it by yourself.
       int blockNumber = 1 + iNumber / 16;
       int iNodeIndex = iNumber % 16;
