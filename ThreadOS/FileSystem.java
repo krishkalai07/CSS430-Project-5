4e6277234f439d3ba2bc4fdc868fdeaec9bc455c
krishkalai07 <krishkalai07@gmail.com>
2020-03-14 22:45:42 -0700

    fix `

--- ThreadOS/FileSystem.java
+++ ThreadOS/FileSystem.java
@@ -3,59 +3,94 @@ public class FileSystem {
     private Directory directory;
     private FileTable filetable;
  
-    public FileSystem( int diskBlocks ) {
+    public FileSystem(int diskBlocks) {
        // create superblock, and format disk with 64 inodes in default
-       superblock = new SuperBlock( diskBlocks );
+       superblock = new SuperBlock(diskBlocks);
  
        // create directory, and register "/" in directory entry 0
-       directory = new Directory( superblock.inodeBlocks );
+       directory = new Directory(superblock.totalInodes);
  
        // file table is created, and store directory in the file table
-       filetable = new FileTable( directory );
+       filetable = new FileTable(directory);
  
        // directory reconstruction
-       FileTableEntry dirEnt = open( "/", "r" );
-       int dirSize = fsize( dirEnt );
-       if ( dirSize > 0 ) {
+       FileTableEntry dirEnt = open("/", "r");
+       int dirSize = fsize(dirEnt);
+       if (dirSize > 0) {
           byte[] dirData = new byte[dirSize];
-          read( dirEnt, dirData );
-          directory.bytes2directory( dirData );
+          read(dirEnt, dirData);
+          directory.bytes2directory(dirData);
        }
-       close( dirEnt );
+       close(dirEnt);
     }
  
-    void sync( ) {
+    //syncs the file system back to the physical disk and write the directory information 
+    public void sync() {
+        //open root with write access
+        FileTableEntry root = open("/", "w");
+        //write to root
+        write(root, directory.directory2bytes());
+        //close root 
+        close(root);
+        //sync superblock
+        superblock.sync();
     }
 
-    boolean format( int files ) {
+    /**
+     * 
+     * 
+     * @param files the maxinum number of files to create.
+     * @return
+     */
+    public boolean format(int files) {
+        //format superblocks
+        superblock.format(files);
+        //create directory,
+        directory = new Directory(superblock.totalInodes);
+        //file table is created and store directory in the file table
+        filetable = new FileTable(directory);
+        //return true once completed
+        return true;
     }
 
-    FileTableEntry open( String filename, String mode ) {
+
+    FileTableEntry open(String filename, String mode) {
+        FileTableEntry fte = filetable.falloc(filename, mode);
+
+        return fte;
+      
     }
 
-    boolean close( FileTableEntry ftEnt ) {
+    boolean close(FileTableEntry ftEnt) {
+        return false;
     }
 
-    int fsize( FileTableEntry ftEnt ) {
+    int fsize(FileTableEntry ftEnt) {
+        return 0;
     }
 
-    int read( FileTableEntry ftEnt, byte[] buffer ) {
+    int read(FileTableEntry ftEnt, byte[] buffer) {
+        return 0;
     }
 
-    int write( FileTableEntry ftEnt, byte[] buffer ) {
+    int write(FileTableEntry ftEnt, byte[] buffer) {
+        return 0;
     }
 
-    private boolean deallocAllBlocks( FileTableEntry ftEnt ) {
+    private boolean deallocAllBlocks(FileTableEntry ftEnt) {
+        return false;
     }
     
-    boolean delete( String filename ) {
+    boolean delete(String filename) {
+        return false;
     }
  
     private final int SEEK_SET = 0;
     private final int SEEK_CUR = 1;
     private final int SEEK_END = 2;
  
-    int seek( FileTableEntry ftEnt, int offset, int whence ) {
+    int seek(FileTableEntry ftEnt, int offset, int whence) {
+        return false;
     }
  }
  
--- ThreadOS/Superblock.java
+++ ThreadOS/Superblock.java
@@ -4,20 +4,41 @@ public class SuperBlock {
    public int freeList;    // the block number of the free list's head
   
    public SuperBlock( int diskSize ) {
-      byte[] blockInfo = new byte[512];
+      byte[] superBlock = new byte[512];
+      SysLib.rawread(0, superBlock);
+      totalBlocks = SysLib.bytes2int(superBlock, 0);
+      totalInodes = SysLib.bytes2int(superBlock, 4);
+      freeList = SysLib.bytes2int(superBlock, 8);
 
-      SysLib.rawread(0, blockInfo);
-
-      totalBlocks = SysLib.bytes2int(blockInfo, 0);
-      if (totalBlocks == 0) { // disk does not contain information
-         // compute values
-         // write to disk
-         // totalBlocks = 
-          
-      } else { // disk already contains the information
-         totalInodes = SysLib.bytes2int(blockInfo, 4);
-         freeList = SysLib.bytes2int(blockInfo, 8);
+      // Validate disk contents
+      if (totalBlocks != diskSize || totalInodes == 0 || freeList < 1) {
+         // Disk data is invalid
+         totalInodes = SysLib.bytes2int(superBlock, 4);
+         freeList = SysLib.bytes2int(superBlock, 8);
       }
-   }
+  }
 
+   public void sync(){ 
+      byte[] superBlock = new byte[Disk.blockSize];
+      SysLib.int2bytes(totalBlocks, superBlock, 0);
+      SysLib.int2bytes(totalInodes, superBlock, 4);
+      SysLib.int2bytes(freeList, superBlock, 8);
+      SysLib.rawwrite(0, superBlock);
+  }
+
+   // write super block to disk
+   /** 
+    * 
+    * @param fileCount number of files to be stored
+    */
+   public void format(int fileCount) { 
+      if (fileCount <= 0) {     
+         totalBlocks = 1;
+         totalInodes = 16;
+      } else {
+         totalInodes = fileCount;
+         totalBlocks = totalInodes / 16;
+      }
+      freeList = 1;
+   }
 }
